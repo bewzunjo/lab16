9e41650d32b65553bbfabac45d706e691412e50a
bewzunjo <saran_kim@cmu.ac.th>
2022-01-14 22:44:05 +0700

    lab16.4

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,30 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &k,int &l,int &m,int &n){
+int e[4];
+int f[4];
+int g = 0, h = 0, i = 0, j = 0;
+e[0] = k, e[1] = l, e[2] = m, e[3] = n;
+g = rand()%4;
+f[0] = e[g];
+e[g] = 0;
+ do{
+	 h = rand()%4;
+ }while(e[h] == 0);
+ f[1] = e[h];
+ e[h] = 0;
+ do{
+	 i = rand()%4;
+ }while(e[i] == 0);
+ f[2] = e[i];
+ e[i] = 0;
+do{
+	j = rand()%4;
+}while(e[j] == 0);
+f[3] = e[j];
+
+k = f[0], l = f[1], m = f[2], n = f[3];
+
+}
