22d4d4a1f63c7e02e9e2bf2aac0e971467d6e11b
bewzunjo <saran_kim@cmu.ac.th>
2022-01-15 12:35:10 +0700

    lab16.5

--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,38 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *k,int *l,int *m,int *n){
+int e[4];
+int f[4];
+int g = 0, h = 0, i = 0, j = 0;
+e[0] = *k, e[1] = *l, e[2] = *m, e[3] = *n;
+g = rand()%4;
+f[0] = e[g];
+e[g] = 0;
+ do{
+	 h = rand()%4;
+ }while(e[h] == 0);
+ f[1] = e[h];
+ e[h] = 0;
+ do{
+	 i = rand()%4;
+ }while(e[i] == 0);
+ f[2] = e[i];
+ e[i] = 0;
+do{
+	j = rand()%4;
+}while(e[j] == 0);
+f[3] = e[j];
+
+*k = f[0], *l = f[1], *m = f[2], *n = f[3];
+
+}
+
+
